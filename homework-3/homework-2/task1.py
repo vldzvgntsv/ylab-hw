7cb0fac8872087e069fc90b6dc8ad16f93e534e9
vldzvgntsv <tetrogum@gmail.com>
2022-07-08 13:42:02 +0300

    added hw-2 part of homework-3

---
+++ homework-3/homework-2/task1.py
@@ -0,0 +1,22 @@
+from custom_range import Range3
+
+
+class CyclicIterator:
+
+    def __init__(self, obj):
+        self.obj = obj
+        self.iterator = iter(self.obj)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return next(self.iterator)
+        except StopIteration:
+            self.iterator = iter(self.obj)
+            return next(self.iterator)
+
+
+for i in CyclicIterator(Range3(5)):
+    print(i)
---
+++ homework-3/homework-2/task2.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Generator, List, Tuple
+
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        for date_period in self.dates:
+            n = 0
+            while date_period[0] + timedelta(days=n) <= date_period[1]:
+                yield date_period[0] + timedelta(days=n)
+                n += 1
+
+
+m = Movie('sw', [
+    (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+    (datetime(2020, 1, 15), datetime(2020, 2, 7))
+])
+
+for d in m.schedule():
+    print(d)
